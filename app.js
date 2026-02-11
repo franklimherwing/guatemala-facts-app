b4e53e13b4a63207637549bb15f6f5bda36bb5c3
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-02-10 22:38:26 -0500

    Refactor showFact, nextFact, and prevFact functions

--- app.js
+++ app.js
@@ -6,28 +6,27 @@ const factText = document.getElementById("factText");
 const card = document.getElementById("card");
 
 function showFact(index){
-card.classList.add("fade-out");
-
-setTimeout(()=>{
-factText.textContent = facts[index];
-card.classList.remove("fade-out");
-},250);
+    card.classList.add("fade-out");
+    setTimeout(()=>{
+        factText.textContent = facts[index];
+        card.classList.remove("fade-out");
+    },250);
 }
 
 function nextFact(){
-currentIndex++;
-if(currentIndex>=facts.length){currentIndex=0;}
-showFact(currentIndex);
+    currentIndex++;
+    if(currentIndex>=facts.length){currentIndex=0;}
+    showFact(currentIndex);
 }
 
 function prevFact(){
-currentIndex--;
-if(currentIndex<0){currentIndex=facts.length-1;}
-showFact(currentIndex);
+    currentIndex--;
+    if(currentIndex<0){currentIndex=facts.length-1;}
+    showFact(currentIndex);
 }
 
 showFact(currentIndex);
 
 if('serviceWorker' in navigator){
-navigator.serviceWorker.register('service-worker.js');
+    navigator.serviceWorker.register('service-worker.js');
 }
