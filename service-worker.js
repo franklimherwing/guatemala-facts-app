c66024c700aeef00e8fe2cab09c923caab797ec4
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-02-10 21:43:15 -0500

    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
+// Guatemalan Facts App - Version 1.1
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open("gtfacts-v1.1").then(cache=>{
+return cache.addAll([
+"./",
+"./index.html",
+"./style.css",
+"./app.js",
+"./facts.js"
+]);
+})
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res||fetch(e.request);
+})
+);
+});
 
