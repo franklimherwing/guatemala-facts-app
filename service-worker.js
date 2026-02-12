083ea9bf2b2e1b6585f99e6046fa30062cb723ed
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-02-12 09:34:26 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,37 @@
-const CACHE_NAME="facts-app-v1.5";
+const CACHE_NAME="guatemala-facts-v1.6";
 
-const FILES=[
+const FILES_TO_CACHE=[
 "./",
-"./index.html",
-"./style.css",
-"./app.js",
-"./facts.js",
-"./manifest.json"
+"index.html",
+"style.css",
+"app.js",
+"facts.js",
+"manifest.json"
 ];
 
-self.addEventListener("install",e=>{
+self.addEventListener("install",event=>{
 self.skipWaiting();
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES))
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE))
 );
 });
 
-self.addEventListener("activate",e=>{
-e.waitUntil(
+self.addEventListener("activate",event=>{
+event.waitUntil(
 caches.keys().then(keys=>{
 return Promise.all(
-keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+keys.filter(k=>k!==CACHE_NAME)
+.map(k=>caches.delete(k))
 );
 })
 );
 self.clients.claim();
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
+self.addEventListener("fetch",event=>{
+event.respondWith(
+caches.match(event.request).then(response=>{
+return response||fetch(event.request);
+})
 );
 });
