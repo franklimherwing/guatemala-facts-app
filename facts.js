e1532d5437b11c4632d9fee45244445b3f293b26
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-02-12 08:31:38 -0600

    Update facts.js

--- facts.js
+++ facts.js
@@ -1,17 +1,18 @@
-const APP_VERSION = "1.4";
+const APP_VERSION="1.5";
 
-const facts = [
+const facts=[
 {
 id:1,
 title:"Ciudad de Guatemala",
 fact:"Capital principal del país.",
 category:"History",
-locationName:"Guatemala City",
-latitude:14.6349,
-longitude:-90.5069,
-imageUrl:"",
-audioUrl:"",
-difficulty:"Easy",
+active:true
+},
+{
+id:2,
+title:"Marimba",
+fact:"Instrumento nacional de Guatemala.",
+category:"Culture",
 active:true
 }
 ];
