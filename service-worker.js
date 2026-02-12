07ac22235ef0a3d4a6358df15c27f1a0712c3b18
franklimherwing <franklim.herwing37@go.cambridgecollege.edu>
2026-02-12 09:02:32 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,18 @@
-const APP_VERSION="1.5";
-const CACHE_NAME="gtfacts-"+APP_VERSION;
+const CACHE_NAME="facts-app-v1.5";
 
 const FILES=[
-"/",
-"/index.html",
-"/style.css",
-"/app.js",
-"/facts.js",
-"/manifest.json"
+"./",
+"./index.html",
+"./style.css",
+"./app.js",
+"./facts.js",
+"./manifest.json"
 ];
 
 self.addEventListener("install",e=>{
 self.skipWaiting();
 e.waitUntil(
-caches.open(CACHE_NAME).then(c=>c.addAll(FILES))
+caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES))
 );
 });
 
@@ -21,11 +20,7 @@ self.addEventListener("activate",e=>{
 e.waitUntil(
 caches.keys().then(keys=>{
 return Promise.all(
-keys.map(k=>{
-if(k!==CACHE_NAME){
-return caches.delete(k);
-}
-})
+keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
 );
 })
 );
@@ -34,6 +29,6 @@ self.clients.claim();
 
 self.addEventListener("fetch",e=>{
 e.respondWith(
-fetch(e.request).catch(()=>caches.match(e.request))
+caches.match(e.request).then(r=>r||fetch(e.request))
 );
 });
